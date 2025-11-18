9af2e86f5ae75af0953268c3656d78b05bdc8cc5
kimmj88 <daialop88@nate.com>
2025-11-18 15:28:51 +0900

    작은 예외처리

--- src/pages/cup/view.vue
+++ src/pages/cup/view.vue
@@ -89,19 +89,13 @@
             <div class="d-flex flex-column">
               <span class="text-body-2 font-weight-medium"> {{ p.nickname }}#{{ p.tagname }} </span>
               <span class="text-caption text-medium-emphasis">
-                {{ p.tier?.name }} · {{ p.tier.point }}pt
+                {{ p.tier?.name }} · {{ p.tier?.point }}pt
               </span>
             </div>
 
             <div class="d-flex flex-column align-end">
-              <v-btn
-                v-if="!cup?.is_confirm"
-                icon
-                size="x-small"
-                variant="text"
-                @click.stop="removePlayer(pos, p.id)"
-              >
-                <v-icon size="16">mdi-close</v-icon>
+              <v-btn icon size="x-small" variant="text" @click.stop="removePlayer(pos, p.id)">
+                <v-icon v-if="!cup?.is_confirm" size="16">mdi-close</v-icon>
               </v-btn>
               <v-icon size="18" class="drag-handle mt-1">mdi-drag-vertical</v-icon>
             </div>
